c109e2315b2e7f0ab2891b3d8910ce8c68f83ef3
npgeorge <17250377+pcsubirachs@users.noreply.github.com>
2022-01-19 22:12:01 -0800

    terminal wallet

--- web_app/test/terminal.py
+++ web_app/test/terminal.py
@@ -12,21 +12,30 @@ print(mnem)
 #derive btc addresses deterministically
 #w = Wallet.create('simp', keys=mnem, network='bitcoin')
 w = Wallet('simp')
-simp_mnem = Wallet.mnemonic()
-print(simp_mnem)
+#simp_mnem = Wallet.mnemonic()
+#print(simp_mnem)
 
-x = 10
+# for loop to generate x amount of addresses
+x = 100
+# for i in range from 0 to x
 for i in range(0, x):
+    # generate a key for the w Wallet
     key = w.get_key(i)
+    # generate an address from that key
     address = key.address
+    # print the addres
     print(address)
 
+
+# getting one key at a time
 #key1 = w.get_key()
 #key2 = w.get_key(1)
 #addr1 = key1.address
 #addr2 = key2.address
 #print(addr1, addr2)
 #
-#
-#addr_list = w.addresslist()[0]
-#print(addr_list)
+
+# getting any address from the list
+addr_list = w.addresslist()[9]
+print('repeat: ', addr_list)
+
