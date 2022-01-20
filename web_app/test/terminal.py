7951364ef4533b0675754e91cf4d6810402abac6
npgeorge <17250377+pcsubirachs@users.noreply.github.com>
2022-01-19 21:58:45 -0800

    terminal

--- web_app/test/terminal.py
+++ web_app/test/terminal.py
@@ -1,5 +1,6 @@
 # using bitcoinlib
 from bitcoinlib.mnemonic import Mnemonic
+from bitcoinlib.wallets import Wallet
 
 # Step 1
 #get mnemonic randomly
@@ -7,16 +8,25 @@ from bitcoinlib.mnemonic import Mnemonic
 mnem = Mnemonic().generate(strength=256, add_checksum=True)
 print(mnem)
 
-# from peter todd python-bitcoinlib
-# import testnet on Bitcoin
-# ‘mainnet’, ‘testnet’, and ‘regtest’ are the available chains from which you can select
-import bitcoin
-#bitcoin.SelectParams('testnet')
-
-import bitcoin.rpc
- 
-proxy = bitcoin.rpc.Proxy()
-print(proxy.getnewaddress())
-
 # Step 2
 #derive btc addresses deterministically
+#w = Wallet.create('simp', keys=mnem, network='bitcoin')
+w = Wallet('simp')
+simp_mnem = Wallet.mnemonic()
+print(simp_mnem)
+
+x = 10
+for i in range(0, x):
+    key = w.get_key(i)
+    address = key.address
+    print(address)
+
+#key1 = w.get_key()
+#key2 = w.get_key(1)
+#addr1 = key1.address
+#addr2 = key2.address
+#print(addr1, addr2)
+#
+#
+#addr_list = w.addresslist()[0]
+#print(addr_list)
